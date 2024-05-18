73f298bbbdec5f4fc9b8263d6749239f191b05a7
Chanwoo Ahn <cwahn@arsvivendi.io>
2024-05-18 13:22:25 +0900

    test: Implement greet example

--- src/athena/base/typeclass.py
+++ src/athena/base/typeclass.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
-from typing import Any, Callable, Generic, Protocol, TypeVar
-from dataclasses import dataclass
+from typing import Any, Callable, Protocol, TypeVar
 
 
 _A = TypeVar("_A")
@@ -10,17 +9,9 @@ _A_co = TypeVar("_A_co", covariant=True)
 _B_co = TypeVar("_B_co", covariant=True)
 
 
-# Fuctor_co = TypeVar("Fuctor_co", bound="Functor", covariant=True)
-# Functor_A = "Functor[A]"
-# Functor_B = "Functor[B]"
-
-# F = TypeVar("F")
-# Fuctor_ = TypeVar("Fuctor_", bound="Functor", covariant=True,)
-
-
 class Functor(Protocol[_A_co]):
     @staticmethod
-    def fmap(f: Callable[[_A], _B], x: Any[_A]) -> Any[_B]: ...
+    def fmap(f: Callable[[_A], _B], x: Any[_A_co]) -> Any[_B]: ...
 
 
 class Applicative(Functor[_A_co], Protocol[_A_co]):
@@ -34,44 +25,3 @@ class Applicative(Functor[_A_co], Protocol[_A_co]):
 class Monad(Applicative[_A_co], Protocol[_A_co]):
     @staticmethod
     def bind(x: Any[_A], f: Callable[[_A], Any[_B]]) -> Any[_B]: ...
-
-
-@dataclass
-class Io(Generic[_A]):
-    action: Callable[[], _A]
-
-    @staticmethod
-    def fmap(f: Callable[[_A], _B], x: Io[_A]) -> Io[_B]:
-        return Io(lambda: f(x.action()))
-
-    @staticmethod
-    def pure(x: _A) -> Io[_A]:
-        return Io(lambda: x)
-
-    @staticmethod
-    def ap(f: Io[Callable[[_A], _B]], x: Io[_A]) -> Io[_B]:
-        return Io(lambda: f.action()(x.action()))
-
-    @staticmethod
-    def bind(x: Io[_A], f: Callable[[_A], Io[_B]]) -> Io[_B]:
-        return Io(lambda: f(x.action()).action())
-
-
-class Random(Generic[_A]):
-    pass
-
-
-def taking_monad(x: Monad[int]) -> bool:
-    return True
-
-Io.fmap(lambda x: x + 1, Io(lambda: 1))
-
-taking_monad(Io(lambda: 1))
-# taking_monad(Random())
-
-
-def taking_fuctor(x: Functor[int]) -> bool:
-    return True
-
-
-taking_fuctor(Io(lambda: 1))
--- src/athena/prelude/seq.py
+++ src/athena/prelude/seq.py
@@ -3,30 +3,30 @@ from typing import Callable, Iterable, Iterator, List, Optional, TypeVar
 import functools
 
 
-A = TypeVar("A")
-B = TypeVar("B")
-C = TypeVar("C")
+_A = TypeVar("_A")
+_B = TypeVar("_B")
+_C = TypeVar("_C")
 
 
 # Sequence operations
 
 
-def filter_map(f: Callable[[A], Optional[A]], xs: Iterable[A]) -> Iterator[A]:
-    for x in xs:
-        if (y := f(x)) is not None:
-            yield y
+# def filter_map(f: Callable[[A], Optional[A]], xs: Iterable[A]) -> Iterator[A]:
+#     for x in xs:
+#         if (y := f(x)) is not None:
+#             yield y
 
 
-def foldl(f: Callable[[A, B], A], acc: A, xs: Iterable[B]) -> A:
-    return functools.reduce(f, xs, acc)
+# def foldl(f: Callable[[A, B], A], acc: A, xs: Iterable[B]) -> A:
+#     return functools.reduce(f, xs, acc)
 
 
-def unique(seq: Iterable[A]) -> List[A]:
-    def add_unique(acc: List[A], item: A) -> List[A]:
-        if item not in acc:
-            acc.append(item)
-        return acc
+# def unique(seq: Iterable[A]) -> List[A]:
+#     def add_unique(acc: List[A], item: A) -> List[A]:
+#         if item not in acc:
+#             acc.append(item)
+#         return acc
 
-    return functools.reduce(add_unique, seq, [])
+#     return functools.reduce(add_unique, seq, [])
 
 # 
