3d3a6d9e32b55ad6945ff661cb89b7b1d75a5460
Chanwoo Ahn <cwahn@arsvivendi.io>
2024-05-25 14:55:33 +0900

    chore: Implement Map with inner dict

--- src/entoli/map.py
+++ src/entoli/map.py
@@ -8,36 +8,81 @@ _A = TypeVar("_A")
 _B = TypeVar("_B")
 
 
-# todo Need extra optimization
+# # todo Need extra optimization
+# @dataclass
+# class Map(Generic[_A, _B]):
+#     inner: list[Tuple[_A, _B]] = field(default_factory=list)
+
+#     def __iter__(self) -> Iterator[Tuple[_A, _B]]:
+#         return self.inner.__iter__()
+
+#     def __contains__(self, item: _A) -> bool:
+#         return item in self.keys()
+
+#     def __getitem__(self, key: _A) -> _B:
+#         for k, v in self.inner:
+#             if k == key:
+#                 return v
+#         raise KeyError(key)
+
+#     def __setitem__(self, key: _A, value: _B):
+#         if key in self.keys():
+#             del self[key]
+
+#         self.inner.append((key, value))
+#         # self.inner.add((key, value))
+
+#     def __delitem__(self, key: _A):
+#         for k, _ in self.inner:
+#             if k == key:
+#                 self.inner.remove((k, _))
+#                 return
+#         raise KeyError(key)
+
+#     def __len__(self):
+#         return len(self.inner)
+
+#     def __bool__(self):
+#         return bool(self.inner)
+
+#     @staticmethod
+#     def from_iter(items: Iterable[Tuple[_A, _B]]) -> "Map[_A, _B]":
+#         return Map(list(items))
+
+#     def items(self) -> Iterable[Tuple[_A, _B]]:
+#         return Seq(lambda: (item for item in self.inner))
+
+#     def keys(self) -> Iterable[_A]:
+#         return Seq(lambda: (k for k, _ in self.inner))
+
+#     def values(self) -> Iterable[_B]:
+#         return Seq(lambda: (v for _, v in self.inner))
+
+
 @dataclass
 class Map(Generic[_A, _B]):
-    inner: list[Tuple[_A, _B]] = field(default_factory=list)
+    inner: dict[_A, _B] = field(default_factory=dict[_A, _B])
+
+    def __init__(self, arg: Iterable[Tuple[_A, _B]] | dict[_A, _B] = dict()):
+        if isinstance(arg, dict):
+            self.inner = arg  # type: ignore
+        else:
+            self.inner = dict(arg)
 
     def __iter__(self) -> Iterator[Tuple[_A, _B]]:
-        return self.inner.__iter__()
+        return iter(self.inner.items())
 
     def __contains__(self, item: _A) -> bool:
-        return item in self.keys()
+        return item in self.inner
 
     def __getitem__(self, key: _A) -> _B:
-        for k, v in self.inner:
-            if k == key:
-                return v
-        raise KeyError(key)
+        return self.inner[key]
 
     def __setitem__(self, key: _A, value: _B):
-        if key in self.keys():
-            del self[key]
-
-        self.inner.append((key, value))
-        # self.inner.add((key, value))
+        self.inner[key] = value
 
     def __delitem__(self, key: _A):
-        for k, _ in self.inner:
-            if k == key:
-                self.inner.remove((k, _))
-                return
-        raise KeyError(key)
+        del self.inner[key]
 
     def __len__(self):
         return len(self.inner)
@@ -45,22 +90,25 @@ class Map(Generic[_A, _B]):
     def __bool__(self):
         return bool(self.inner)
 
+    def __repr__(self) -> str:
+        return repr(self.inner)
+
     @staticmethod
-    def from_iter(items: Iterable[Tuple[_A, _B]]) -> "Map[_A, _B]":
-        return Map(list(items))
+    def from_iter(items: Iterable[Tuple[_A, _B]]) -> Map[_A, _B]:
+        return Map(items)
 
     def items(self) -> Iterable[Tuple[_A, _B]]:
-        return Seq(lambda: (item for item in self.inner))
+        return Seq(lambda: (item for item in self.inner.items()))
 
     def keys(self) -> Iterable[_A]:
-        return Seq(lambda: (k for k, _ in self.inner))
+        return Seq(lambda: (k for k in self.inner.keys()))
 
     def values(self) -> Iterable[_B]:
-        return Seq(lambda: (v for _, v in self.inner))
+        return Seq(lambda: (v for v in self.inner.values()))
 
 
-def test_Map_construct():
-    m = Map.from_iter([(1, 2), (3, 4)])
+def _test_Map_construct():
+    m = Map({1: 2, 3: 4})
     assert len(m) == 2
     assert 1 in m
     assert 2 not in m
@@ -71,8 +119,8 @@ def test_Map_construct():
     assert list(m.values()) == [2, 4]
 
 
-def test_Map_set():
-    m = Map.from_iter([(1, 2), (3, 4)])
+def _test_Map_set():
+    m = Map({1: 2, 3: 4})
     m[1] = 3
     assert len(m) == 2
     assert m[1] == 3
@@ -81,8 +129,8 @@ def test_Map_set():
     assert m[5] == 6
 
 
-def test_Map_del():
-    m = Map.from_iter([(1, 2), (3, 4)])
+def _test_Map_del():
+    m = Map({1: 2, 3: 4})
     del m[1]
     assert len(m) == 1
     assert 1 not in m
@@ -93,17 +141,17 @@ def test_Map_del():
     assert 1 not in m
 
 
-def test_Map_iter():
-    m = Map.from_iter([(1, 2), (3, 4)])
+def _test_Map_iter():
+    m = Map({1: 2, 3: 4})
     assert list(m) == [(1, 2), (3, 4)]
 
 
-def test_Map_bool():
-    m = Map.from_iter([(1, 2), (3, 4)])
+def _test_Map_bool():
+    m = Map({1: 2, 3: 4})
     assert m
-    m = Map.from_iter([])
+    m = Map({})
     assert not m
-    m = Map.from_iter([(1, 2)])
+    m = Map({1: 2})
     assert m
     del m[1]
     assert not m
