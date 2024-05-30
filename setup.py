6e58d863e6ac939206ede387ff11a83037e41a5f
Chanwoo Ahn <cwahn@arsvivendi.io>
2024-05-30 13:48:58 +0900

    build: Remove unused runtime deps

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="entoli",
-    version="0.1.0",
+    version="0.1.1",
     author="Your Name",
     author_email="cwahn0904@gmail.com",
     description="A Python functional programming library",
@@ -16,10 +16,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
-    install_requires=[
-        # Add your package dependencies here, e.g., 'requests', 'numpy'
-    ],
+    python_requires=">=3.12",
+    install_requires=[],
     extras_require={
         "dev": open("requirements_dev.txt").read().splitlines(),
     },
