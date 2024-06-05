689d499bb0ba61d4f26080b78315993e9b7c190e
Chanwoo Ahn <cwahn@arsvivendi.io>
2024-06-05 09:55:01 +0900

    chore: Draft http client

--- src/entoli/http_client.py
+++ src/entoli/http_client.py
@@ -1,66 +1,32 @@
-# from enum import Enum
-# from http.client import HTTPConnection, HTTPResponse
+from ast import parse
+from enum import Enum
+from http.client import HTTPConnection, HTTPResponse
 
-# from dataclasses import dataclass
-# from typing import Dict, Literal
-# from entoli.base.io import Io
-
-
-# def get_conn(host: str, post: int = 80) -> Io[HTTPConnection]:
-#     return Io(lambda: HTTPConnection(host, post))
+from dataclasses import dataclass
+from typing import Dict, Literal
+from entoli.base.io import Io
 
 
-# type Method = Literal["GET", "POST", "PUT", "PATCH", "DELETE"]
+def get_conn(host: str, post: int = 80) -> Io[HTTPConnection]:
+    return Io(lambda: HTTPConnection(host, post))
 
 
-# def send_request_raw(
-#     conn: HTTPConnection,
-#     method: str,
-#     path: str,
-#     body: str = "",
-#     headers: dict[str, str] = {},
-# ) -> Io[HTTPResponse]:
-#     conn.request(method, path, body, headers=headers)
-#     return Io(lambda: conn.getresponse())
+type Method = Literal["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
-# # def raw_get_request(
-# #     conn: HTTPConnection, path: str, headers: dict[str, str] = {}
-# # ) -> Io[HTTPResponse]:
-# #     conn.request("GET", path, headers=headers)
-# #     return Io(lambda: conn.getresponse())
+def request(
+    conn: HTTPConnection,
+    method: str,
+    path: str,
+    body: str = "",
+    headers: dict[str, str] = {},
+) -> Io[HTTPResponse]:
+    conn.request(method, path, body, headers=headers)
+    return Io(lambda: conn.getresponse())
 
 
-# # def raw_post_request(
-# #     conn: HTTPConnection, path: str, data: str, headers: dict[str, str] = {}
-# # ) -> Io[HTTPResponse]:
-# #     conn.request("POST", path, data, headers=headers)
-# #     return Io(lambda: conn.getresponse())
-
-
-# # def raw_put_request(
-# #     conn: HTTPConnection, path: str, data: str, headers: dict[str, str] = {}
-# # ) -> Io[HTTPResponse]:
-# #     conn.request("PUT", path, data, headers=headers)
-# #     return Io(lambda: conn.getresponse())
-
-
-# # def raw_patch_request(
-# #     conn: HTTPConnection, path: str, data: str, headers: dict[str, str] = {}
-# # ) -> Io[HTTPResponse]:
-# #     conn.request("PATCH", path, data, headers=headers)
-# #     return Io(lambda: conn.getresponse())
-
-
-# # def raw_delete_request(
-# #     conn: HTTPConnection, path: str, headers: dict[str, str] = {}
-# # ) -> Io[HTTPResponse]:
-# #     conn.request("DELETE", path, headers=headers)
-# #     return Io(lambda: conn.getresponse())
-
-
-# def close_conn(conn: HTTPConnection) -> Io[None]:
-#     return Io(lambda: conn.close())
+def close_conn(conn: HTTPConnection) -> Io[None]:
+    return Io(lambda: conn.close())
 
 
 # @dataclass
@@ -82,5 +48,8 @@
 #     method: Method,
 #     path: str,
 #     body: str = "",
-# ) -> Io[HTTPResponse]:
-#     ...
+# ) -> Io[HTTPResponse]: ...
+
+
+def parse_cookies(header_str: str) -> Dict[str, str]:
+    return dict(cookie.split("=") for cookie in header_str.split(";"))
