14115efbbe067205a96d721d0a7c8b8e48ab386e
Chanwoo Ahn <cwahn@arsvivendi.io>
2024-06-05 11:57:47 +0900

    chore: Draft http client

--- src/entoli/http_client.py
+++ src/entoli/http_client.py
@@ -1,4 +1,5 @@
 from ast import parse
+from calendar import c
 from enum import Enum
 from http.client import HTTPConnection, HTTPResponse
 
@@ -14,7 +15,7 @@ def get_conn(host: str, post: int = 80) -> Io[HTTPConnection]:
 type Method = Literal["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
-def request(
+def raw_request(
     conn: HTTPConnection,
     method: str,
     path: str,
@@ -29,27 +30,38 @@ def close_conn(conn: HTTPConnection) -> Io[None]:
     return Io(lambda: conn.close())
 
 
-# @dataclass
-# class HttpSession:
-#     conn: HTTPConnection
-#     cookies: Io[Dict[str, str]] = Io(lambda: {})
+@dataclass
+class HttpSession:
+    conn: HTTPConnection
+    cookies: Io[Dict[str, str]] = Io(lambda: {})
 
 
-# def get_session(host: str, port: int = 80) -> Io[HttpSession]:
-#     return get_conn(host, port).map(lambda conn: HttpSession(conn))
+def get_session(host: str, port: int = 80) -> Io[HttpSession]:
+    return get_conn(host, port).map(lambda conn: HttpSession(conn))
 
 
-# def session_from_conn(conn: HTTPConnection) -> Io[HttpSession]:
-#     return Io(lambda: HttpSession(conn))
-
-
-# def send_request(
-#     session: HttpSession,
-#     method: Method,
-#     path: str,
-#     body: str = "",
-# ) -> Io[HTTPResponse]: ...
+def session_from_conn(conn: HTTPConnection) -> Io[HttpSession]:
+    return Io(lambda: HttpSession(conn))
 
 
 def parse_cookies(header_str: str) -> Dict[str, str]:
     return dict(cookie.split("=") for cookie in header_str.split(";"))
+
+
+def request(
+    session: HttpSession,
+    method: Method,
+    path: str,
+    body: str = "",
+) -> Io[HTTPResponse]:
+    def _request():
+        conn = session.conn
+        cookies = session.cookies.action()
+        headers = {**cookies, "Content-Type": "application/json"}
+        conn.request(method, path, body, headers=headers)
+        res = conn.getresponse()
+        new_cookies = parse_cookies(res.getheader("Set-Cookie", ""))
+        session.cookies = Io(lambda: {**cookies, **new_cookies})
+        return res
+    
+    return Io(_request)
